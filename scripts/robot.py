1a15af06c0aaa5b4f6d49bf379c66c5ea88bda58
jkllbn2563 <jkllbn25632yahoo.com.tw>
2019-08-01 00:08:05 +0800

    the google second=5

--- scripts/robot.py
+++ scripts/robot.py
@@ -20,7 +20,8 @@ rate=rospy.Rate(1)
 def listen():
 	with speech_recognition.Microphone() as source:
 		r.adjust_for_ambient_noise(source, duration=0.5)
-		audio=r.listen(source)
+		audio=r.record(source,duration=5)
+		#audio=r.listen(source)
 		#print("You said " + r.recognize_google(audio,language='zh-TW'))
 		print("You said " + r.recognize_google(audio,language='en-US'))
 
